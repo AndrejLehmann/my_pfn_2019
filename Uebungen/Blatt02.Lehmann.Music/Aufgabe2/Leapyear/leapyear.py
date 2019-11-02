d43e7d514ebc59085b1279129361c089a19c6bce
Andrej Lehmann <alehmann@physnet.uni-hamburg.de>
2019-11-02 18:12:20 +0100

    Ordner Umbenannt

--- Uebungen/Blatt02.Lehmann.Music/Aufgabe2/Leapyear/leapyear.py
+++ Uebungen/Blatt02.Lehmann.Music/Aufgabe2/Leapyear/leapyear.py
@@ -7,8 +7,8 @@ import sys
 for year in sys.argv[1:]:
     year = int(year)
     if (year%4 == 0) and (year%100 != 0) or (year%400 == 0):
-        print(year, " ist ein Schaltjahr")
+        print(year, "ist ein Schaltjahr")
     else:
-        print(year, " ist kein Schaltjahr")
+        print(year, "ist kein Schaltjahr")
 
 
