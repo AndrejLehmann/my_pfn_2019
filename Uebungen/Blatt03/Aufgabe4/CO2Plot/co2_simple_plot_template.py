eb261226e9831b6a061b04a86ea843fb72993d87
Andrej Lehmann <alehmann@physnet.uni-hamburg.de>
2019-11-06 14:40:03 +0100

    Blatt03

---
+++ Uebungen/Blatt03/Aufgabe4/CO2Plot/co2_simple_plot_template.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import sys
+plt.switch_backend('agg') # to allow remote use
+
+jahre = [1971,1975,1980,1985,1990,1995,2000,2005,2010,2016]
+
